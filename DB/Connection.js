a7c142d44371a433db9462c8c024ede0a6fb6295
Dowglas Barros <dowglasrodrigo@gmail.com>
2021-05-17 02:12:30 -0300

    Fix: Heroku config vars

--- DB/Connection.js
+++ DB/Connection.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+const dbUser = 'process.env.DB_USER';
+const dbPassword = 'process.env.DB_PASSWORD';
 
-const URI = 'mongodb+srv://${{ secrets.DB_USER }}:${{ secrets.DB_PASSWORD }}@cluster0.7v3yi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const URI = `mongodb+srv://${dbUser}:${dbPassword}@cluster0.7v3yi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const connectDB = async() => {
   await mongoose.connect(URI, {
